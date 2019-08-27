6043541e65962864317b5171cd1e91daa950bbef
Kaputin <kaputinevgeny@gmail.com>
2019-08-27 21:24:05 +0300

    Задание 3.2

--- templates/main.php
+++ templates/main.php
@@ -29,8 +29,9 @@
                             <span class="lot__amount">Стартовая цена</span>
                             <span class="lot__cost"><?=format_price($val['price']); ?></span>
                         </div>
-                        <div class="lot__timer timer">
-                            12:23
+                        <?php $res=format_date($val['dtclose']); ?>
+                        <div class="lot__timer timer <?php if ($res[0] == "00"): ?>timer--finishing<?php endif ?>">
+                            <?=$res[0]; ?>:<?=$res[1]; ?>
                         </div>
                     </div>
                 </div>
